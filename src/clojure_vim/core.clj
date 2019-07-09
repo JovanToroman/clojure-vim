83be25f999a4bfe7e874501f95e8a193559f160b
Toroman Jovan <toromanj@gmail.com>
2019-07-09 21:57:56 +0200

    Implement Clojure Vim CLI

--- src/clojure_vim/core.clj
+++ src/clojure_vim/core.clj
@@ -1,26 +1,25 @@
 (ns clojure-vim.core
   (:gen-class))
+(use '[clojure.string :only [trim-newline]]
+     '[clojure.java.shell :only [sh]])
 
 (defn -main
   []
-  (for [i [1 2 3]]
-    (let [file-name (str "test" i ".txt")]
-      ; Create a temporary empty file
+    (doseq [i [1 2 3]]
+    (let [file-name (str "test" i ".txt")
+          working-directory (trim-newline (:out (sh "printenv" "PWD")))]
       (spit file-name "")
 
-      ; Now I need to run vim, so I can edit file-name interactively.
+      (let [process-builder (java.lang.ProcessBuilder. (list "vim" (str working-directory "/" file-name)))
+            inherit (java.lang.ProcessBuilder$Redirect/INHERIT)]
+      (.redirectOutput process-builder inherit)
+      (.redirectError process-builder inherit)
+      (.redirectInput process-builder inherit)
 
-      ; YOUR CODE TO RUN vim GOES HERE
-
-      ; After I close vim, this script execution should continue,
-      ; so I can do some processing of file-name's new content (isn't relevant to this task)
-  )))
-
-; So basically after I compile the project and run
-; java -jar target/uberjar/clojure-vim-0.1.0-standalone.jar
-; I should be presented with vim to edit test1.txt, then test2.txt, and finally test3.txt.
-
-; I don't need a cover letter,
-; but when applying for the job add "hey-ho" text as your cover letter or in a private comment,
-; so I know that you read the whole core.clj.
-; Applications without "hey-ho" text won't be considered.
+      (.waitFor (.start process-builder))
+      )
+      ;additional processing here
+    )
+    )
+    (System/exit 0)
+    )
